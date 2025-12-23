81112eb52ce8efcdbb70f39d9abad49047c0e96a
adhira <adhiradeogade@gmail.com>
2025-12-23 20:26:25 +0530

    what I understood

--- socketserver.js
+++ socketserver.js
@@ -1,23 +1,3 @@
-// const { Server } = require("socket.io");
-// const io = new Server()
-// io.on("connection", (socket) => {
-//       console.log('Socket connected');
-//     // fs.readFile(__dirname + '/home.html', (err, html) => {
-//     //     if (err) {
-//     //     res.writeHead(404, { 'Content-Type': 'text/plain' });
-//     //     res.end('404 File Not Found');
-//     //     } else {
-//     //     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     //     res.write(html);
-//     //     res.end();
-//     //     }
-//     // })
-// });
-
-// io.listen(3000).on('connection', () => {
-//     console.log('connection 2');
-// });
-
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const fs = require('fs')
@@ -39,23 +19,26 @@ const io = new Server(httpServer, {});
 io.on("connection", (socket) => {
     console.log("Inside io.on...")
     // Listen for messages from the client
-    socket.on('message', (msg) => {
-        console.log('Received message:', msg);
-        io.emit('message', `Server received: ${msg}`);
+    // socket.on('message', (msg) => {
+    //     console.log('Received message:', msg);
+    //     io.emit('message', `Server received: ${msg}`);
 
-        // socket.emit('message', `Server received: ${msg}`);
-        // socket.broadcast.emit('hi');
-        io.emit('chat message', msg);
-    });
+    //     // socket.emit('message', `Server received: ${msg}`);
+    //     // socket.broadcast.emit('hi');
+    //     io.emit('chat message', msg);
+    // });
 
     socket.on('cursor-position', (evt) => {
         console.log('cursor-position', evt);
+        // Broadcast is used for sending the message to all the clients
+        // except the source client
         socket.broadcast.emit('cursor-position', {...evt, id: socket.id})
     });
     
     // On client disconnect
     socket.on('disconnect', () => {
         console.log('Client disconnected');
+        // io.emit is used to send the message to all the clients
         io.emit('cursor-disconnect', {id: socket.id})
     });
 });
@@ -64,4 +47,3 @@ httpServer.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
 
-// httpServer.listen(3000);
